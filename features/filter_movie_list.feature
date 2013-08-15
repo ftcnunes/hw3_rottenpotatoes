dc30b969c91a713feb7196d49ae91d40ce181fb3
uxico <ftcnunes@gmail.com>
2013-08-16 00:55:43 +0100

    modified:   features/filter_movie_list.feature 	modified:   features/sort_movie_list.feature 	modified:   features/step_definitions/movie_steps.rb

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -19,17 +19,24 @@ Background: movies have been added to database
   | Raiders of the Lost Ark | PG     | 12-Jun-1981  |
   | Chicken Run             | G      | 21-Jun-2000  |
 
-  And  I am on the RottenPotatoes home page
+  And I am on the RottenPotatoes home page
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  Given I check the following ratings: "PG, R"
   # enter step(s) to uncheck all other checkboxes
+  Given I uncheck the following ratings: "G, PG-13"
   # enter step to "submit" the search form on the homepage
+  And I press 'Refresh'
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should see movies with rating: "PG, R"
   # enter step(s) to ensure that other movies are not visible
+  Then I should not see movies with rating: "PG-13, G"
 
 Scenario: no ratings selected
   # see assignment
 
 Scenario: all ratings selected
   # see assignment
+  Given I check all ratings
+  Then I should see all of the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,15 +14,73 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  webpage = page.body
+  webpage.index(e1).should < webpage.index(e2)
+end
+
+Then /^I should see movies ordered by: "(.*?)"/ do |order|
+  if order == "Title"
+    movies = Movie.find(:all, :order => 'title')
+    
+    for i in 0..movies.length-2
+      step "I should see " + "\"" + movies[i][:title] + "\"" + " before " + "\"" + movies[i+1][:title] + "\""
+    end
+  elsif order == "Release Date"
+    movies = Movie.find(:all, :order => 'release_date')
+
+    for i in 0..movies.length-2
+      step "I should see " + "\"" + movies[i][:title] + "\"" + " before " + "\"" + movies[i+1][:title] + "\""
+    end
+  end
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+When /I (un)?check the following ratings: "(.*)"/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  aux = Array.new
+  aux = rating_list.split(%r{,\s*})
+  
+  if uncheck.nil?
+    aux.each do |rating|
+      check("ratings_#{rating}")
+    end
+  else
+    aux.each do |rating|
+      uncheck("ratings_#{rating}")
+    end
+  end
+end
+
+Given /^I press 'Refresh'$/ do
+  click_button("ratings_submit")
+end
+
+Then /^I should( not)? see movies with rating: "(.*)"/ do |not_see, rating_list|
+  aux = Array.new
+  aux = rating_list.split(%r{,\s*})
+  
+  movies = Movie.find(:all, :conditions => {:rating => aux}).map { |movie| movie.title }
+  
+  if not_see.nil?
+    movies.each do |movie|
+      step "I should see " + "\"" + movie + "\""
+    end
+  else
+    movies.each do |movie|
+      step "I should not see " + "\"" + movie + "\""
+    end
+  end
+end
+
+Then /^I should see all of the movies$/ do
+  Movie.count.should == page.all("#movies tbody tr").size
+end
+
+Given /^I check all ratings$/ do
+  # Dummy
 end
